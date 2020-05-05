09962745b2c82db7fb263f862a481c09fb36cf8a
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-05-05 18:23:58 -0300

    metodo put 2.0

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const data = [
+    { id: 1, name: "mike", age: 22, company: "Nascetur Mus Company"},
+    { id: 2, name: "Eleanor", age: 42, company: "Hendrerit Donec LLP"},
+    { id: 3, name: "Dylan", age: 51, company: "Nisi Incorporated"},
+    { id: 4, name: "Leila", age: 30, company: "Eros Non Limited"},
+    { id: 5, name: "Jason", age: 31, company: "Accumsan Interdum Associates"}
+]
+
 app.use(express.json())
-//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Metodo GET
 app.get('/', (req, res) => {
@@ -19,11 +27,11 @@ app.post('/', (req, res) => {
     return res.json([site, name])
 })
 // metodo PUT
-//http://localhost:3000/:identificador
-app.put('/:identificador', (req, res) => {
-    const identificador = req.params.identificador
-    console.log(identificador)
-    return res.json([identificador])
+//http://localhost:3000/:id
+app.put('/:id', (req, res) => {
+    const id = req.params.id
+    const item = data.find(item => item.id == id)
+    return res.json([item])
 })
 
 app.listen(port, () => {
